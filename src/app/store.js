d97e6b0b530cc5ac7fd488c01c3a289e2995f36e
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-06-28 19:26:25 +0700

    Revert "add redux"
    This reverts commit 50769d4b9825ec74a30e06852374ae1dfbc0715c.

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,6 @@
-import {
-  configureStore
-} from "@reduxjs/toolkit";
-import userReducer from "../slices/userSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
+    reducer: {
+    },
+  });
