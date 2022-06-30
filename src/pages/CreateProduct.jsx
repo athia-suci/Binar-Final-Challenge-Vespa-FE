e0a75c1fcca9d14c6b7acee5a10e9d345dcbc34c
krisnapramulyap <19560029@stmik.jayakarta.ac.id>
2022-07-01 01:11:00 +0700

    update styling + isLogin checking

--- src/pages/CreateProduct.jsx
+++ src/pages/CreateProduct.jsx
@@ -1,21 +1,19 @@
-// import { NavbarProduct } from "../components/navbar";
-import React, { useRef, useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import "../css/style.css";
-import {
-    Col,
-    Container,
-    Row,
-    Form,
-    Button,
-    Alert,
-    FloatingLabel,
-} from "react-bootstrap";
+import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Col, Row, Nav, Navbar, Form, Container, Button, Alert } from "react-bootstrap";
+import { useNavigate, Navigate, Link } from "react-router-dom";
+import { selectUser } from "../slices/userSlice";
+import { FiArrowLeft } from "react-icons/fi";
+import { BiPlus } from "react-icons/bi";
 import axios from "axios";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import "../css/style.css";
 
-export default function InfoProduct() {
+export default function CreateProduct() {
     const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const userRedux = useSelector(selectUser);
+    const [user, setUser] = useState(userRedux.creds);
     const nameField = useRef("");
     const priceField = useRef("");
     const categoryField = useRef("");
@@ -33,6 +31,13 @@ export default function InfoProduct() {
         message: "",
     });
 
+    const colourButton = {
+        backgroundColor: '#7126B5',
+        borderRadius: '16px',
+    }
+    const borderRadius = {
+        borderRadius: '16px',
+    }
 
     useEffect(() => {
         if (image) {
@@ -44,16 +49,40 @@ export default function InfoProduct() {
         } else {
             setPreview(null);
         }
-    }, [image]);
+        const fetchData = async () => {
+            try {
+                // Check status user login
+                // 1. Get token from localStorage
+                const token = localStorage.getItem("token");
+
+                // 2. Check token validity from API
+                const currentUserRequest = await axios.get(
+                    "http://localhost:2000/v1/users",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                const currentUserResponse = currentUserRequest.data;
+
+                if (currentUserResponse.status) {
+                    setUser(currentUserResponse.data.user);
+                }
+            } catch (err) {
+                setIsLoggedIn(false);
+            }
+        };
+        fetchData();
+    }, [image])
 
     const onPost = async (e) => {
         e.preventDefault();
 
         try {
             const token = localStorage.getItem("token");
-
             const postPayload = new FormData();
-
             postPayload.append("name", nameField.current.value);
             postPayload.append("price", priceField.current.value);
             postPayload.append("category", categoryField.current.value);
@@ -88,114 +117,100 @@ export default function InfoProduct() {
         }
     };
 
-    return (
-        <>
-            {/* <NavbarProduct></NavbarProduct> */}
-            <Container className="justify-content-center mt-5">
-                <Row>
-                    <div className="col-3 text-center">
-                        <Link to="/daftarJual">
-                            <Button variant="light">
-                                <AiOutlineArrowLeft className="mb-1" />
+    return isLoggedIn ? (
+        <div>
+            {/* navbar */}
+            <div className="na1 py-4 shadow">
+                <nav className="navbar navbar-expand-lg navbar-light bg-all">
+                    <Link to="/">
+                        <button className="na2 navbar-brand box"></button>
+                    </Link>
+                    <Navbar.Brand href="#" className="brand" />
+                    <div className="offcanvas-body" id="offcanvasRight">
+                        <div className="info1 navbar">
+                            <Nav className="text-dark"> Buat  Detail Produk </Nav>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <Container className="my-5 w-50">
+                <div>
+                    <Link className="arrow2" to="/" style={{ color: "black" }}>
+                        <FiArrowLeft />
+                    </Link>
+                </div>
+                <div>
+                    <Nav className="info3 text-dark">Lengkapi Detail Produk</Nav>
+                </div>
+                <Form onSubmit={onPost}>
+                    <Form className="border1 mb-3" style={{ fontWeight: "bold" }}>
+                        <Form.Label>Nama Produk</Form.Label>
+                        <Form.Control style={borderRadius} type="text" ref={nameField} placeholder="Nama" />
+                    </Form>
+                    <Form className="border1 mb-3" style={{ fontWeight: "bold" }}>
+                        <Form.Label>Harga Produk</Form.Label>
+                        <Form.Control style={borderRadius} type="text" ref={priceField} placeholder="Rp 0,00" />
+                    </Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Kategori</Form.Label>
+                        <Form.Select style={borderRadius} ref={categoryField} aria-label="Default select example">
+                            <option>Pilih Kategori</option>
+                            <option value="Hobi">Hobi</option>
+                            <option value="Kendaraan">Kendaraan</option>
+                            <option value="Baju">Baju</option>
+                            <option value="Elektronik">Elektronik</option>
+                            <option value="Kesehatan">Kesehatan</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3" style={{ fontWeight: "bold" }}>
+                        <Form.Label>Deskripsi</Form.Label>
+                        <Form.Control
+                            style={borderRadius}
+                            type="text"
+                            ref={descriptionField}
+                            placeholder="Contoh: Jalan Ikan Hiu 33"
+                            as="textarea"
+                            rows={3}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" style={{ fontWeight: "bold" }}>
+                        Foto Produk
+                    </Form.Group>
+                    <Form.Control
+                        type="file"
+                        class="form-control-file"
+                        id="exampleFormControlFile1"
+                        ref={fileInputRef}
+                        accept="image/*"
+                        onChange={(e) => {
+                            const file = e.target.files[0];
+                            if (file && file.type.substr(0, 5) === "image") {
+                                setImage(file);
+                            } else {
+                                setImage(null);
+                            }
+                        }}
+                        hidden
+                    />
+                    <Row>
+                        <Col>
+                            <Button style={colourButton} onClick={(e) => setIsPublished(false)} className="myButton7 w-100" type="submit">
+                                Preview
                             </Button>
-                        </Link>
-                    </div>
-                    <div className="col-6">
-                        <Form onSubmit={onPost} id="infoProduct" className="">
-                            <Form.Group className="mb-3">
-                                <Form.Label>Nama Produk</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Contoh: johndee@gmail.com"
-                                    className="radius-primary"
-                                    ref={nameField}
-                                // defaultValue={products.name}
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Harga Produk</Form.Label>
-                                <Form.Control
-                                    type="harga"
-                                    placeholder="Rp 0,00"
-                                    className="radius-primary"
-                                    ref={priceField}
-                                // defaultValue={products.price}
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label>Kategori</Form.Label>
-                                <Form.Select ref={categoryField} aria-label="Default select example">
-                                    <option>Pilih Kategori</option>
-                                    <option value="Hobi">Hobi</option>
-                                    <option value="Kendaraan">Kendaraan</option>
-                                    <option value="Baju">Baju</option>
-                                    <option value="Elektronik">Elektronik</option>
-                                    <option value="Keseheatan">Keseheatan</option>
-                                </Form.Select>
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicText">
-                                <Form.Label>Deskripsi</Form.Label>
-                                <textarea
-                                    class="form-control"
-                                    placeholder="Contoh: Jalan Ikan Hiu 33"
-                                    rows="3"
-                                    ref={descriptionField}
-                                // defaultValue={products.description}
-                                ></textarea>
-                            </Form.Group>
-                            <p>Foto Produk</p>
-                            <form-group className="mb-3">
-                                <Form.Label
-                                    className="upload-button-product"
-                                    for="exampleFormControlFile1"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        fileInputRef.current.click();
-                                    }}
-                                >
-                                    {preview ? (
-                                        <img src={preview} onClick={() => setImage(null)} alt="preview" />
-                                    ) : ("")}
-                                </Form.Label>
-
-                                <Form.Control
-                                    type="file"
-                                    class="form-control-file"
-                                    id="exampleFormControlFile1"
-                                    ref={fileInputRef}
-                                    accept="image/*"
-                                    onChange={(e) => {
-                                        const file = e.target.files[0];
-                                        if (file && file.type.substr(0, 5) === "image") {
-                                            setImage(file);
-                                        } else {
-                                            setImage(null);
-                                        }
-                                    }}
-                                    hidden
-                                />
-
-                            </form-group>
-                            <div className="mb-3 d-flex">
-                                <Button
-                                    className=" w-50 radius-primary bg-color-secondary"
-                                    type="submit"
-                                    onClick={(e) => setIsPublished(false)}
-                                >
-                                    Preview
-                                </Button>
-                                <Button
-                                    className=" w-50 radius-primary bg-color-secondary"
-                                    type="submit"
-                                    onClick={(e) => setIsPublished(true)}
-                                >
-                                    Terbitkan
-                                </Button>
-                            </div>
-                        </Form>
-                    </div>
-                </Row>
+                        </Col>
+                        <Col>
+                            <Button style={colourButton} onClick={(e) => setIsPublished(true)} className="myButton6 w-100" type="submit">
+                                Terbitkan
+                            </Button>
+                        </Col>
+                    </Row>
+                    {errorResponse.isError && (
+                        <Alert variant="danger">{errorResponse.message}</Alert>
+                    )}
+                </Form>
             </Container>
-        </>
-    );
+        </div>
+    ) : (
+        <Navigate to="/login" replace />);
 }
